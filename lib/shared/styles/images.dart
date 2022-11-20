5e35801cd95dfa0af7718b25f42204bafa4e6347
PedroHenriqueDevBR <p.lyma18@gmail.com>
2022-11-20 15:41:13 -0300

    chore: added country flags

--- lib/shared/styles/images.dart
+++ lib/shared/styles/images.dart
@@ -1,4 +1,38 @@
 class AppImages {
   final String logo = 'assets/images/logo.png';
   final String curvedBackground = 'assets/images/curved_background.png';
+
+  // Flag Images
+  final String flagBase = 'assets/flags';
+  final String catar = 'assets/flags/bandeira-catar.jpg';
+  final String equador = 'assets/flags/equador.jpg';
+  final String holanda = 'assets/flags/holanda.jpeg';
+  final String senegal = 'assets/flags/senegal.jpeg';
+  final String estadosUnidos = 'assets/flags/estados-unidos.jpg';
+  final String inglaterra = 'assets/flags/bandeira-inglaterra.jpg';
+  final String ira = 'assets/flags/ira.jpg';
+  final String paisDeGales = 'assets/flags/bandeira-pais-de-gales.jpg';
+  final String arabiaSaudita = 'assets/flags/arabia-saudita.jpeg';
+  final String mexico = 'assets/flags/mexico.jpg';
+  final String polonia = 'assets/flags/bandeira-polonia.jpg';
+  final String australia = 'assets/flags/australia.jpeg';
+  final String dinamarca = 'assets/flags/dinamarca.jpg';
+  final String franca = 'assets/flags/franca.jpg';
+  final String tunisia = 'assets/flags/tunisia.jpg';
+  final String alemanha = 'assets/flags/alemanha.jpeg';
+  final String costaRica = 'assets/flags/costa-rica.jpg';
+  final String espanha = 'assets/flags/espanha.jpg';
+  final String japao = 'assets/flags/japao.jpg';
+  final String belgica = 'assets/flags/belgica.jpg';
+  final String canada = 'assets/flags/canada.jpeg';
+  final String croacia = 'assets/flags/croacia.jpg';
+  final String marrocos = 'assets/flags/marrocos.jpg';
+  final String brasil = 'assets/flags/brasil.jpeg';
+  final String camaroes = 'assets/flags/camaroes.jpg';
+  final String suica = 'assets/flags/suica.jpeg';
+  final String servia = 'assets/flags/servia.jpg';
+  final String coreiaDoSul = 'assets/flags/bandeira-coreia-do-sul.jpg';
+  final String gana = 'assets/flags/gana.jpg';
+  final String portugal = 'assets/flags/portugal.jpg';
+  final String uruguai = 'assets/flags/uruguai.jpg';
 }
